fa9448ccf6a44670704eb347a8ca85e4171450c0
rkota <rosairekota@gmail.com>
2021-11-09 15:58:48 +0100

    feat:add fillable property and belongsToMany in  Project Model

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -2,10 +2,17 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Technology;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Project extends Model
 {
     use HasFactory;
+
+    protected $fillable=['title','description','image_url'];
+
+    public function technologies(){
+        return $this->belongsToMany(Technology::class);
+    }
 }
