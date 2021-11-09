f83081a46ee32aa7dfe749e72efbbc47933c2e25
rkota <rosairekota@gmail.com>
2021-11-09 16:02:01 +0100

    feat:add fillable property and belongsToMany in  Technology Model

--- app/Models/Technology.php
+++ app/Models/Technology.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Technology extends Model
 {
     use HasFactory;
+    protected $fillable=['name','image_url'];
+
+    public function projects(){
+        return $this->belongsToMany(Project::class);
+    }
 }
